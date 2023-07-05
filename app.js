6145300451b95e7c38a7ec95611224ecc7d08e1c
Stephen <stephen.s.espinosa@gmail.com>
2023-07-05 23:37:01 +0200

    added value

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 mailchimp.setConfig({
-  apiKey: process.env.key,
+  apiKey: process.env.KEY,
   server: "us12",
 });
 
@@ -38,7 +38,7 @@ app.post("/", function (req, res) {
   // us12 comes from API key, changed the ${}
   const url = "https://us12.api.mailchimp.com/3.0/lists/7201a4dab7";
 
-  const authenticate = `stephen:${process.env.key}`;
+  const authenticate = `stephen:${process.env.KEY}`;
   console.log(authenticate);
 
   // adding options for our http request > method:POST, authenticaion: any name:api key
